bcbe769812f180c01efd74fc01be8d0cd789de50
txiribimakula <pablotobalina@gmail.com>
2021-02-07 12:50:00 +0100

    Basic slides.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Reveal from 'reveal.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'learn-revealjs';
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+    Reveal.initialize();
+  }
 }
